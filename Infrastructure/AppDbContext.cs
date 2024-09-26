8e369b0c9ece9ec0eed52112660433796d1aa524
mohamadreza-amini <mohamadrezaaminidehaghi@gmail.com>
2024-09-26 20:21:28 +0330

    .

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+            
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> option):base(option) { }
+       
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Market;TrustServerCertificate=True;Integrated Security=SSPI;");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+    }
+}
---
+++ Infrastructure/Contracts/ICommentRepository.cs
@@ -0,0 +1,35 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contracts
+{
+    public interface ICommentRepository
+    {
+        Task<Comment> CreateComment(Comment comment);
+        Task<bool> DeleteComment(Guid comment);
+        Task<Comment> UpdateComment(Comment comment);
+
+        Task<Comment> GetComment(Guid Id);
+        Task<Comment> GetComment(Expression<Func<Comment,bool>> predicate);
+        Task<IQueryable<Comment>> GetComments(Expression<Func<Comment,bool>> predicate);
+    }
+
+
+
+
+    public interface ICommentRepositorya<T> where T : BaseEntity<Guid>
+    {
+        Task<T> CreateComment(T comment);
+        Task<bool> DeleteComment(Guid comment);
+        Task<T> UpdateComment(Comment comment);
+
+        Task<T> GetComment(Guid Id);
+        Task<T> GetComment(Expression<Func<T, bool>> predicate);
+        Task<IQueryable<T>> GetComments(Expression<Func<T, bool>> predicate);
+    }
+}
