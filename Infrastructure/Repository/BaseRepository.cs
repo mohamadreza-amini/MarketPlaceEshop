b460cc773a2e8e71f322445cec5b1ce14f262c42
mohamadreza-amini <mohamadrezaaminidehaghi@gmail.com>
2024-10-02 16:18:30 +0330

    add base repository

--- Infrastructure/Repository/BaseRepository.cs
+++ Infrastructure/Repository/BaseRepository.cs
@@ -1,54 +1,82 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Infrastructure.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Model.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.Repository
+namespace Infrastructure.Repository;
+
+public class BaseRepository<T, KeyTypeId> : IBaseRepository<T, KeyTypeId> where T : BaseEntity<KeyTypeId> where KeyTypeId : struct
 {
+    private readonly AppDbContext _appDbContext;
+    private readonly DbSet<T> _entitySet;
 
-  /*  public class BaseRepository<T, KeyTypeId> : IEntityTypeConfiguration<T> where T : BaseEntity<KeyTypeId> where KeyTypeId : struct
+    public BaseRepository(AppDbContext appDbContext)
     {
-        protected bool GeneratedValueForKry { get; set; } = true;
-        protected bool UseForTraceable { get; set; } = false;
-        protected bool RequireTraceable { get; set; } = false;
+        _appDbContext = appDbContext;
+        _entitySet = appDbContext.Set<T>();
+    }
 
-        public virtual void Configure(EntityTypeBuilder<T> builder)
+    public async Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null)
+    {
+        if (predicate == null)
         {
-
-
-            builder.HasKey(x => x.Id);
-            if (GeneratedValueForKry)
-            {
-                builder.Property(x => x.Id).ValueGeneratedOnAdd();
-            }
-
-            if (UseForTraceable)
-            {
-                builder.Ignore(x => x.CreateDatetime);
-                builder.Ignore(x => x.UpdateDatetime);
-                builder.Ignore(x => x.AdminCreated);
-                builder.Ignore(x => x.AdminCreatedId);
-                builder.Ignore(x => x.AdminUpdated);
-                builder.Ignore(x => x.AdminUpdatedId);
-            
-
-            }
-            else
-            {
-                builder.Property(x => x.CreateDatetime).IsRequired(RequireTraceable);
-                builder.Property(x => x.UpdateDatetime).IsRequired(RequireTraceable);
-                builder.HasOne(x => x.AdminCreated).WithMany().HasForeignKey(x => x.AdminCreatedId).IsRequired(RequireTraceable).OnDelete(DeleteBehavior.NoAction);
-
-                builder.HasOne(x => x.AdminUpdated).WithMany().HasForeignKey(x => x.AdminUpdatedId).IsRequired(RequireTraceable).OnDelete(DeleteBehavior.NoAction);
-
-
-
-            }
-
+            return await Task.Run(() => _entitySet.AsQueryable());
         }
-    }*/
+
+        return await Task.Run(() => _entitySet.Where(predicate));
+    }
+
+    public async Task<IQueryable<TResult>> GetAll<TResult>(Expression<Func<T, TResult>> selector, Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool isNoTracking = true)
+    {
+        IQueryable<T> query = _entitySet;
+        query = isNoTracking == true ? query.AsNoTracking() : query.AsQueryable();
+        query = predicate != null ? query.Where(predicate) : query;
+        query = include != null ? include(query) : query;
+        return query != null && query.Any() ? query.Select(selector) : default;
+
+    }
+
+    public async Task<T> GetAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _entitySet.FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<T> GetByIdAsync(KeyTypeId id)
+    {
+        return await GetAsync(x => x.Id.Equals(id));
+    }
+
+    public async Task<T> CreateDataAsync(T data)
+    {
+        await _entitySet.AddAsync(data);
+        return data;
+    }
+
+    public async Task<bool> DeleteDataAsync(KeyTypeId id)
+    {
+        var data = await GetAsync(x => x.Id.Equals(id));
+        if (data != null)
+        {
+            _entitySet.Remove(data);
+            await CommitAsync();
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<T> UpdateDataAsync(T data)
+    {
+        _entitySet.Update(data);
+        await CommitAsync();
+        return data;
+    }
+
+    public async Task<int> CommitAsync() => await _appDbContext.SaveChangesAsync();
+
 }
---
+++ Infrastructure/Repository/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Interfaces;
+
+public interface IBaseRepository<T, KeyTypeId> where T : BaseEntity<KeyTypeId> where KeyTypeId : struct
+{
+    Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null);
+    Task<IQueryable<TResult>> GetAll<TResult>(Expression<Func<T, TResult>> selector, Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool isNoTracking = true);
+    Task<T> GetByIdAsync(KeyTypeId id);
+    Task<T> GetAsync(Expression<Func<T, bool>> predicate);
+    Task<T> CreateDataAsync(T data);
+    Task<T> UpdateDataAsync(T data);
+    Task<bool> DeleteDataAsync(KeyTypeId id);
+    Task<int> CommitAsync();
+}
