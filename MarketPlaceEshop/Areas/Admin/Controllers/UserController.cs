6b7fa2638a4c743e03882580720552d6f0659eb2
mohamadreza-amini <mohamadrezaaminidehaghi@gmail.com>
2024-12-23 01:40:35 +0330

    Add a new admin registration page

--- MarketPlaceEshop/Areas/Admin/Controllers/UserController.cs
+++ MarketPlaceEshop/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataTransferObject.DTOClasses.Person.Commands;
+using Microsoft.AspNetCore.Mvc;
 using Service.ServiceInterfaces.PersonServices;
+using Shared;
 using Shared.Enums;
 
 namespace MarketPlaceEshop.Areas.Admin;
@@ -7,10 +9,11 @@ namespace MarketPlaceEshop.Areas.Admin;
 public class UserController : Controller
 {
     private readonly ISupplierService _supplierService;
-
-    public UserController(ISupplierService supplierService)
+    private readonly IAdminService _adminService;
+    public UserController(ISupplierService supplierService, IAdminService adminService)
     {
         _supplierService = supplierService;
+        _adminService = adminService;
     }
 
     public IActionResult Index()
@@ -20,18 +23,44 @@ public class UserController : Controller
 
     public async Task<IActionResult> MembershipRequest(CancellationToken cancellation, int pageIndex = 1, int pageSize = 10)
     {
-       var SuppliersDto = await _supplierService.GetAllSuppliersbyStatusAsync(ConfirmationStatus.Unchecked, cancellation, pageIndex, pageSize);
+        var SuppliersDto = await _supplierService.GetAllSuppliersbyStatusAsync(ConfirmationStatus.Unchecked, cancellation, pageIndex, pageSize);
         return View(SuppliersDto);
     }
 
 
-    public async Task ConfirmSupplier( Guid supplierId, CancellationToken cancellation)
+    public async Task ConfirmSupplier(Guid supplierId, CancellationToken cancellation)
     {
         await _supplierService.ChangeSupplierStatusAsync(supplierId, ConfirmationStatus.Confirmed, cancellation);
     }
 
-    public async Task RejectSupplier( Guid supplierId, CancellationToken cancellation)
+    public async Task RejectSupplier(Guid supplierId, CancellationToken cancellation)
     {
         await _supplierService.ChangeSupplierStatusAsync(supplierId, ConfirmationStatus.Rejected, cancellation);
+    }
+
+
+
+
+    [HttpGet]
+    public async Task<IActionResult> RegisterAdmin()
+    {      
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> RegisterAdmin(UserCommand userDto, CancellationToken cancellation)
+    {
+        if (!ModelState.IsValid) 
+            return RedirectToAction("RegisterAdmin", "User", new {area="Admin"});
+        userDto.DateOfBirth = userDto.DateOfBirth.ToSolar();
+
+        try
+        {
+            var result = await _adminService.CreateAsync(userDto, cancellation);
+            TempData["ToastSuccess"] = "ادمین جدید با موفقیت ثبت شد";
+        }
+        catch (Exception ex) {
+            TempData["ToastError"] = "خطایی رخ داده";
+        }
+        return View();
     }
 }
